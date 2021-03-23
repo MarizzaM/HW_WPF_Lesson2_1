fc8c9a28be6704c3f9031f6a84ef9215393d4262
User <User@DESKTOP-0BJFLEG>
2021-03-23 14:49:51 +0200

    Add project files.

---
+++ HW_WPF_Lesson2_1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_WPF_Lesson2_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            result.Text = (Int32.Parse(firstNumber.Text) + Int32.Parse(secondNumber.Text)).ToString();
+        }
+    }
+}
